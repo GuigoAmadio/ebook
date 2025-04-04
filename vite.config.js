12b0c85df855aeaa11e9e3bc1546d7b8f4edbdc4
Guillermo <guillermoguigo840@hotmail.com>
2025-04-03 22:58:34 -0300

    aaa

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/ebook/", // 👈 MUITO IMPORTANTE
+  //  base: "/ebook/", // 👈 MUITO IMPORTANTE
   plugins: [react()],
 });
